060b6e65338470a712906031e77bc6188360f245
Paul Willworth <ioscode@gmail.com>
2022-12-18 14:22:56 -0800

    Add old events cleanup maintenance

--- maintenance.py
+++ maintenance.py
@@ -7,15 +7,50 @@ import logging
 import logging.handlers
 import os
 
+# Delete saved reports and their data files older than a timestamp
 def cleanReports(beforeTimestamp):
+    cleanCount = 0
     con = db.aConn()
     cur = con.cursor()
     cur.execute("SELECT account, startDate, endDate, transactionsContent, reportContent FROM reports WHERE proc=0 and generatedTimestamp < %s", (beforeTimestamp,))
     row = cur.fetchone()
     while row != None:
         db.deleteReport(row[0], row[1], row[2], row[3], row[4])
+        cleanCount += 1
         row = cur.fetchone()
     con.close()
+    logging.info('Cleaned up {0} old reports'.format(cleanCount))
+
+# Delete saved transaction data for any account that has not run a report recently enough to still be saved
+def cleanTransactions():
+    accounts = []
+    cleanAccounts = []
+    con = db.aConn()
+    # Get list of accounts with active reports
+    with con.cursor() as cur:
+        cur.execute("SELECT account FROM reports")
+        row = cur.fetchone()
+        while row != None:
+            accounts.append(row[0])
+            row = cur.fetchone()
+    logging.info('{0} accounts have active reports: {1}'.format(len(accounts), str(accounts)))
+    # Get list of accounts that have saved transaction data excluding tavern type so we dont bother with accts that have only activity from sales crawler
+    with con.cursor() as cur2:
+        cur2.execute("SELECT DISTINCT account FROM transactions WHERE eventType != 'tavern'")
+        row = cur2.fetchone()
+        while row != None:
+            if row[0] not in accounts:
+                cleanAccounts.append(row[0])
+            row = cur2.fetchone()
+    logging.info('{0} accounts have saved transactions'.format(len(cleanAccounts)))
+    # clean tx data for inactive accounts except tavern sale/hires populated by block crawler
+    with con.cursor() as cur3:
+        for account in cleanAccounts:
+            cur3.execute("DELETE FROM transactions WHERE account=%s and (eventType != 'tavern' or fee != 0)", (account,))
+            logging.info('Cleaned {0} transactions for account {1}'.format(cur3.rowcount, account))
+    
+    con.close()
+
 
 def main():
     # get in the right spot when running this so file paths can be managed relatively
@@ -27,6 +62,7 @@ def main():
     maxAgeDate = datetime.datetime.now() - datetime.timedelta(days=settings.MAX_REPORT_AGE_DAYS)
     logging.warning('Cleaning report records older than {0}'.format(maxAgeDate.isoformat()))
     cleanReports(datetime.datetime.timestamp(maxAgeDate))
+    cleanTransactions()
 
 
 if __name__ == "__main__":
