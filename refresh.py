df389735b0bfbdf522b67722fe9cff7fb2ea0ca7
Paul Willworth <ioscode@gmail.com>
2023-07-01 21:07:13 -0700

    price cleanup util script

--- refresh.py
+++ refresh.py
@@ -4,14 +4,18 @@ import db
 import contracts
 from web3 import Web3
 import prices
+import json
 import logging
 import logging.handlers
 import jsonpickle
 
 def main():
-    handler = logging.handlers.RotatingFileHandler('../refresh.log', maxBytes=33554432, backupCount=10)
+    handler = logging.handlers.RotatingFileHandler('refresh.log', maxBytes=33554432, backupCount=10)
     logging.basicConfig(handlers=[handler], level=logging.INFO, format='%(asctime)s.%(msecs)03d %(levelname)-8s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
     logging.info('Starting refresh process')
+    priceNetwork()
+
+def populateFees():
     # Connect to right network that txs are for
     w3 = Web3(Web3.HTTPProvider(nets.hmy_web3))
     if not w3.isConnected():
@@ -69,6 +73,52 @@ def addFee(w3, con, tx, eventType, events, account):
     else:
         logging.info('skipping tx {0}, does not meet update criteria'.format(tx))
 
+def priceNetwork():
+    for k, v in contracts.HARMONY_TOKENS.items():
+        logging.info('Clean {0} {1} prices'.format('harmony', v))
+        cleanPrices(k, v, 'harmony')
+    for k, v in contracts.DFKCHAIN_TOKENS.items():
+        logging.info('Clean {0} {1} prices'.format('dfkchain', v))
+        cleanPrices(k, v, 'dfkchain')
+    for k, v in contracts.KLAYTN_TOKENS.items():
+        logging.info('Clean {0} {1} prices'.format('klaytn', v))
+        cleanPrices(k, v, 'klaytn')
+
+def cleanPrices(token, tokenName, network):
+    # temp set price networks
+    currentDate = ''
+    goodPriceFound = False
+    goodPriceData = []
+    priceRemovals = []
+    con = db.aConn()
+    with con.cursor() as cur:
+        cur.execute("SELECT token, date, prices FROM prices WHERE token=%s ORDER BY date", (token,))
+        row = cur.fetchone()
+        while row != None:
+            if row[1] != currentDate:
+                with con.cursor() as cur2:
+                    for item in priceRemovals:
+                        logging.info('delete {0}'.format(str(item)))
+                        cur2.execute("DELETE FROM prices WHERE date=%s AND token=%s AND prices=%s", (item[0], item[1], item[2]))
+
+                currentDate = row[1]
+                goodPriceFound = False
+                priceRemovals = []
+
+            priceData = json.loads(row[2])
+            if (goodPriceFound == True and [row[1], row[0], row[2]] != goodPriceData) or priceData['usd'] < 0.000001 or priceData['usd'] > 1000:
+                logging.info('tag for removal {0} - {1} - {2}'.format(row[1], tokenName, priceData['usd']))
+                priceRemovals.append([row[1], row[0], row[2]])
+            else:
+                logging.info('keep good price {0} - {1} - {2}'.format(row[1], tokenName, priceData['usd']))
+                goodPriceData = [row[1], row[0], row[2]]
+                goodPriceFound = True
+            row = cur.fetchone()
+    with con.cursor() as cur2:
+        for item in priceRemovals:
+            logging.info('delete {0}'.format(str(item)))
+            cur2.execute("DELETE FROM prices WHERE date=%s AND token=%s AND prices=%s", (item[0], item[1], item[2]))
+    con.close()
 
 if __name__ == "__main__":
 	main()
